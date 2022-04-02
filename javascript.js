26463ea8b0bf2fd50b71bfe2b3c1e7975160ceb2
abertjames <96638271+abertjames@users.noreply.github.com>
2022-04-02 01:51:30 -0400

    make default board 16x16
    added commented out sample code for later reference

--- javascript.js
+++ javascript.js
@@ -1,6 +1,16 @@
 const button = document.querySelector('.my-button');
 const board = document.querySelector(".board");
 button.addEventListener("click", handleClick);
+
+function genBoard () {
+    board.innerHTML='';
+    for (let i = 0; i<16**2; i++) {
+        board.innerHTML +='<div class="square"></div>';
+    }
+    board.style.setProperty("--grid-size", 16);
+}
+genBoard()
+
 function handleClick() {
   board.innerHTML = '';
   for (let i = 0; i < 9; i++) {
@@ -8,3 +18,14 @@ function handleClick() {
   }
   board.style.setProperty("--grid-size", 3);
 }
+
+// var n=4;//take grid column value as you want
+
+// for(var i = 0; i < n; i++) {
+//     document.body.innerHTML+='<div class="row">';
+
+//     for(j = 0; j < n; j++) {
+//         document.body.innerHTML+='<div class="gridsquare">' + (i*5+j+1) + '</div>';
+//     }
+
+//     document.body.innerHTML+='</div>';
