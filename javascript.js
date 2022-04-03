e8ebfced6b95a64b934298a30860987db7e65592
abertjames <96638271+abertjames@users.noreply.github.com>
2022-04-02 22:05:55 -0400

    generate grid

--- javascript.js
+++ javascript.js
@@ -1,42 +1,70 @@
-// const button = document.querySelector('.my-button');
-const board = document.querySelector(".board");
-// button.addEventListener("click", handleClick);
+const gridContainer = document.querySelector("#gridContainer");
 
-function genBoard (n) {
-    board.innerHTML='';
-    for (let i = 0; i<n**2; i++) {
-        board.innerHTML +='<div class="square"></div>';
+const div = document.createElement('div');
+div.classList.add('cell');
+
+function genGrid (n) {
+    for (let i = 0; i<n; i++) {
+
+        const rowContainer = document.createElement('div');
+        rowContainer.classList.add('row');
+
+        for (let j = 0; j<n; j++ ) {
+            const div = document.createElement('div');
+            div.classList.add('cell');
+            rowContainer.appendChild(div);
+        }
+        gridContainer.appendChild(rowContainer);
     }
-    board.style.setProperty("--grid-size", n);
 }
-genBoard()
+genGrid(16)
 
-// function handleClick(n) {
-//   board.innerHTML = '';
-//   for (let i = 0; i < n**2; i++) {
-//     board.innerHTML +='<div class="square"></div>';
-//   }
-//   board.style.setProperty("--grid-size", n);
-// }
 
 let slider = document.getElementById("myRange");
 let output = document.getElementById("para");
 output.textContent = slider.value;
 
-
+let n = slider.value;
 slider.oninput = function() {
     output.textContent = slider.value;
-    let n = slider.value;
+    n = slider.value;
     genBoard(n)
 }
 
-// var n=4;//take grid column value as you want
 
-// for(var i = 0; i < n; i++) {
-//     document.body.innerHTML+='<div class="row">';
 
-//     for(j = 0; j < n; j++) {
-//         document.body.innerHTML+='<div class="gridsquare">' + (i*5+j+1) + '</div>';
+
+
+
+// // const button = document.querySelector('.my-button');
+// const board = document.querySelector(".board");
+// // button.addEventListener("click", handleClick);
+
+// function genBoard (n) {
+//     board.innerHTML='';
+//     for (let i = 0; i<n**2; i++) {
+//         board.innerHTML +='<div class="square"></div>';
+//         document.createElement('div');
 //     }
+//     board.style.setProperty("--grid-size", n);
+// }
+// genBoard()
 
-//     document.body.innerHTML+='</div>';
+// // function handleClick(n) {
+// //   board.innerHTML = '';
+// //   for (let i = 0; i < n**2; i++) {
+// //     board.innerHTML +='<div class="square"></div>';
+// //   }
+// //   board.style.setProperty("--grid-size", n);
+// // }
+
+// // var n=4;//take grid column value as you want
+
+// // for(var i = 0; i < n; i++) {
+// //     document.body.innerHTML+='<div class="row">';
+
+// //     for(j = 0; j < n; j++) {
+// //         document.body.innerHTML+='<div class="gridsquare">' + (i*5+j+1) + '</div>';
+// //     }
+
+// //     document.body.innerHTML+='</div>';
