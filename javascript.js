b0086b307467815e5571c84eb354e538414433d8
abertjames <96638271+abertjames@users.noreply.github.com>
2022-04-05 16:31:12 -0400

    adds function to find cells near clicked cell

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const gridContainer = document.querySelector("#gridContainer");
 
 function genGrid (n) {
+
     //resets the grid every time it is generated
     gridContainer.innerHTML = ""; 
 
@@ -10,7 +11,9 @@ function genGrid (n) {
         rowContainer.classList.add('row');
 
         for (let j = 0; j<n; j++ ) {
+
             const div = document.createElement('div');
+
             //generates a unique ID for the cell so it can be referenced specifically
             div.setAttribute('id', `${i}-${j}`);   
             div.classList.add('cell');
@@ -82,12 +85,18 @@ function mouseUp () {
     downStatus = false;
 }
 
+//this function checks the status of the user input settings and calls the appropriate function
+function eventHandler () {
+    
+}
+
 function paint (nodeID) {
     const div = document.getElementById(`${nodeID}`);
     div.classList.add("black-paint")
 }
 
-//check up, down, left, right positions of selected cell
+//check up, down, left, right positions of selected cell stored in proximalCells
+let proximalCells=[];
 function getProximal (e) {
 
     //gets the i and j positions of the clicked cell
@@ -95,11 +104,54 @@ function getProximal (e) {
     let nodeID_i = nodeID.slice(0,nodeID.indexOf('-'));
     let nodeID_j = nodeID.slice(nodeID.indexOf('-')+1);
 
-    //gets the cell locations of the four cells surrounding the clicked node
+    //gets the cell id's of the four cells surrounding the clicked node
     let nodeUp = document.getElementById(`${nodeID_i}-${+nodeID_j+1}`);
     let nodeDown = document.getElementById(`${nodeID_i}-${+nodeID_j-1}`);
     let nodeRight = document.getElementById(`${+nodeID_i+1}-${nodeID_j}`);
     let nodeLeft = document.getElementById(`${+nodeID_i-1}-${nodeID_j}`);
+    let node = document.getElementById(nodeID);
 
-    console.log(nodeUp, nodeDown, nodeRight, nodeLeft)
+    proximalCells = [node, nodeUp, nodeDown, nodeRight, nodeLeft]
+    // return proximalCells
+    checkProximal(proximalCells,cellsToColor);
+}
+
+//this function will check whether the cells in quadArray are already colored, 
+//out of bounds, or already in the list of cells
+let cellsToColor = [];
+function checkProximal (toCheck,toColor) {
+    
+    proximalCells = toCheck;
+    cellsToColor = toColor;
+
+    //this checks to see if the user clicked on a node which already has a class on it 
+    //ie an edge or already colored cells.
+    //this would not work for the fill function because it is supposed to fill in blank space.
+    //if it fails this test it will not produce a cellsToColor array. however, if it passes
+    //this check we would want to include it in the list to color
+    if (proximalCells[0].classList.length != 1){ 
+        return
+    }
+
+    for (i=0 ; i < proximalCells.length ; i++) {
+        
+        //check to see if quadArray contains null or already colored elements
+        if (proximalCells[i] === null) {
+            continue
+
+        //check to see if cell already has color class on it
+        } else if (proximalCells[i].classList.length != 1) {
+            continue
+
+        //check to see if cell is already logged in cellsToColor
+        } else if (cellsToColor.includes(proximalCells[i])) {
+            continue
+
+        //adds all cells that pass the checks to the list to be colored
+        } else {
+            cellsToColor = cellsToColor.push(proximalCells[i]);
+        }
+
+    }
+    // return cellsToColor
 }
